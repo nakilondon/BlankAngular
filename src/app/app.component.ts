d4513d74a86a092ce47368e2bd4938192fac916a
James Lewes <james.lewes@worldpay.com>
2019-11-21 22:23:54 +0000

    new repo

---
+++ src/app/app.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pm-root',
+  template: `
+    <nav class='navbar navbar-expand navbar-light bg-light'>
+      <a class='navbar-brand'>{{pageTitle}}</a>
+      <ul class='navbar-nav'>
+        <li class='nav-item'><a class='nav-link' routerLinkActive='active'
+              [routerLink]="['/welcome']">Home</a>
+        </li>
+        <li class='nav-item'><a class='nav-link' routerLinkActive='active' [routerLinkActiveOptions]="{exact: true}"
+              [routerLink]="['/zones']">Zone List</a>
+        </li>
+        <li class='nav-item'><a class='nav-link' routerLinkActive='active' [routerLinkActiveOptions]="{exact: true}"
+              [routerLink]="['/zones/0/edit']">Add Zone</a>
+        </li>
+      </ul>
+    </nav>
+    <div class='container'>
+      <router-outlet></router-outlet>
+    </div>
+    `,
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  pageTitle = 'Home Heating';
+}
